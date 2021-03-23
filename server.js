2c08fa7ec834be4bf9bbf5cfa9cbaf5fe68cb533
Jim Beaumont <jameskevinbeaumont@gmail.com>
2021-03-23 16:18:31 -0400

    Additional changes for heroku deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 // Load Config
 dotenv.config({ path: './config/config.env' });
@@ -12,8 +13,22 @@ const apiRoute = require('./routes/api');
 
 const app = express();
 
-// CORS
-app.use(cors());
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.heroku'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        console.log("** Origin of request " + origin)
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            console.log("Origin acceptable")
+            callback(null, true)
+        } else {
+            console.log("Origin rejected")
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
+//app.use(cors());
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -26,6 +41,15 @@ app.use('/user', userRoute);
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 };
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 8081
 
